14af27749ed896187ee58db3084c4b89611e89bf
solxire88 <136970695+solxire88@users.noreply.github.com>
2024-10-10 16:34:29 +0100

    fixed path

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ export default function App() {
           }
         />
         <Route
-          path="/"
+          path="/science"
           element={
             <Science
               addToCartL={(e) => addToCart(e, cart)}
