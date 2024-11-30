f7b7ccab33cf7a9fef793051927c8a92746c5b74
Solaire <136970695+solxire88@users.noreply.github.com>
2024-11-30 15:12:36 +0100

    Update newData.js

--- src/newData.js
+++ src/newData.js
@@ -2,7 +2,7 @@ export const data = [
   {
     Book: "On The Shortness Of Life",
     author: "Seneca",
-    price: "750 DA",
+    price: "680 DA",
     image: "https://i.imgur.com/9PVxlHh.jpeg",
     descp:
       "The Stoic writings of the philosopher Seneca, who lived from c. 5 BC to AD 65, offer powerful insights into the art of living, the importance of reason and morality, and continue to provide.",
@@ -11,7 +11,7 @@ export const data = [
   {
     Book: "Discourses and Selected Writings",
     author: "Epictetus",
-    price: "825 DA",
+    price: "950 DA",
     image: "https://i.imgur.com/htuilJO.jpeg",
     descp:
       "A new translation of the influential teachings of the great Stoic philosopher.",
@@ -20,8 +20,8 @@ export const data = [
   {
     Book: "Meditations",
     author: "Marcus Aurelius",
-    price: "850 DA",
-    image: "https://i.imgur.com/ZReg2or.jpeg",
+    price: "950 DA",
+    image: "https://i.imgur.com/z0OZuGM.jpeg",
     descp:
       "A clear and eloquent leading translation of the Roman emperor’s reflections on human life and death.",
     tags: ["Philosophy", "Stoicism"],
@@ -29,7 +29,7 @@ export const data = [
   {
     Book: "Letters From A Stoic",
     author: "Seneca",
-    price: "850 DA",
+    price: "950 DA",
     image: "https://i.imgur.com/lcGoYoy.jpeg",
     descp:
       '"It is philosophy that has the duty of protecting us...without it no one can lead a life free of fear or worry."',
@@ -38,7 +38,7 @@ export const data = [
   {
     Book: "Pensées Pour Moi Même",
     author: "Marc Aurèle",
-    price: "850 DA",
+    price: "950 DA",
     image: "https://i.imgur.com/lVEnUVd.png",
     descp:
       "Pensées pour moi-même est un recueil de réflexions intimes de Marc Aurèle, où il explore des idées stoïciennes sur la maîtrise de soi, l'acceptation du destin et la quête de la vertu. Ce texte offre des conseils pratiques pour vivre avec sagesse, sérénité et justice face aux défis de la vie.",
@@ -47,8 +47,8 @@ export const data = [
   {
     Book: "Meditations On First Philosophy",
     author: "Rene Descartes",
-    price: "750 DA",
-    image: "https://i.imgur.com/Fkpjf7f.jpg", // Updated to a unique image for consistency
+    price: "680 DA",
+    image: "https://i.imgur.com/Fkpjf7f.jpg",
     descp:
       "Descartes's Meditations on First Philosophy, the fundamental and originating work of the modern era in Western philosophy, is presented here in Donald Cress's completely revised edition of his well-established translation, bringing this version even closer to Descartes's original, while maintaining its clear and accessible style.",
     tags: ["Philosophy", "Non-Fiction", "Classics", "France"],
@@ -56,26 +56,17 @@ export const data = [
   {
     Book: "El Escape Cubano",
     author: "Mira Canion",
-    price: "750 DA",
-    image: "https://i.imgur.com/uNPpT5N.jpg", // Updated to a unique image
+    price: "680 DA",
+    image: "https://i.imgur.com/uNPpT5N.jpg",
     descp:
       "Inspired by a true story, El Escape Cubano is a first-person narrative reader with 140 unique words. In a matter of minutes, Miguel's happiness changes to gripping fear when he overhears his parents fighting about escaping from Cuba on a raft.",
     tags: ["Spanish Literature", "School", "Language Learning"],
   },
-  // {
-  //   Book: "Manuel D'Épictète",
-  //   author: "Épictète",
-  //   price: "825 DA",
-  //   image: "https://i.imgur.com/0qC2a8Z.jpeg", // Updated to a unique image
-  //   descp:
-  //     "Le Manuel d'Épictète est un guide de sagesse stoïcienne, offrant des principes pour mener une vie vertueuse. Il enseigne l'importance de distinguer ce qui dépend de nous de ce qui échappe à notre contrôle. Son objectif ultime est d'atteindre la paix intérieure par la maîtrise de soi.",
-  //   tags: ["Philosophie", "Stoicisme"],
-  // },
   {
     Book: "Beyond Good and Evil",
     author: "Friedrich Nietzsche",
-    price: "850 DA",
-    image: "https://i.imgur.com/4qT4bOh.jpg", // Updated to a unique image
+    price: "950 DA",
+    image: "https://i.imgur.com/4qT4bOh.jpg",
     descp:
       "Nietzsche's exploration of morality and the concept of 'beyond good and evil', focusing on individual freedom, power, and societal values.",
     tags: ["Philosophy", "Non-Fiction", "Classics", "German"],
@@ -83,8 +74,8 @@ export const data = [
   {
     Book: "The Almanack Of Naval Ravikant",
     author: "Eric Jorgenson",
-    price: "850 DA",
-    image: "https://i.imgur.com/iVPC41B.jpeg", // Updated to a unique image
+    price: "950 DA",
+    image: "https://i.imgur.com/iVPC41B.jpeg",
     descp:
       "The Almanack of Naval Ravikant offers a collection of insights and principles from the entrepreneur and philosopher on how to build wealth and cultivate long-term happiness, emphasizing that these are learnable skills rather than mere luck or innate traits.",
     tags: ["Business", "Non-Fiction", "Self Help", "Philosophy", "Psychology"],
@@ -92,8 +83,8 @@ export const data = [
   {
     Book: "The Seven Year Slip",
     author: "Eric Jorgenson",
-    price: "850 DA",
-    image: "https://i.imgur.com/lIgqsHm.jpeg", // Updated to a unique image
+    price: "950 DA",
+    image: "https://i.imgur.com/lIgqsHm.jpeg",
     descp:
       "An overworked book publicist with a perfectly planned future hits a snag when she falls in love with her temporary roommate…only to discover he lives seven years in the past, in this witty and wise new novel from the New York Times bestselling author of The Dead Romantics.",
     tags: ["Romance", "Fiction", "Self Help", "Fantasy"],
@@ -101,8 +92,8 @@ export const data = [
   {
     Book: "Le Manuel",
     author: "Épictète",
-    price: "750 DA",
-    image: "https://i.imgur.com/sccEggv.png", // Updated to a unique image
+    price: "680 DA",
+    image: "https://i.imgur.com/sccEggv.png",
     descp:
       "'Le Manuel d'Épictète' est un guide stoïcien qui enseigne à vivre en paix avec soi-même et le monde, en se concentrant sur ce qui dépend de nous (pensées, actions) et en acceptant ce qui échappe à notre contrôle. Un classique de sagesse et de sérénité.",
     tags: ["Philosophie", "Non-Fiction", "Stoicisme"],
@@ -110,20 +101,37 @@ export const data = [
   {
     Book: "This Is Marketing",
     author: "Seth Goden",
-    price: "850 DA",
-    image: "https://i.imgur.com/wz2XFLa.png", // Updated to a unique image
+    price: "950 DA",
+    image: "https://i.imgur.com/wz2XFLa.png",
     descp:
       "'This Is Marketing' by Seth Godin is a must-read for anyone looking to create impactful, customer-focused marketing. It highlights the importance of building trust, understanding your audience, and offering value that truly resonates. With practical insights, it shifts the focus from mass appeal to meaningful connections that drive lasting change.",
     tags: ["Marketing", "Non-Fiction", "Self Developement"],
   },
-   {
+  {
     Book: "White Nights",
     author: "Fyodor Dostoyevsky",
-    price: "750 DA",
-    image: "https://i.imgur.com/rv9RLuB.jpeg", // Updated to a unique image
+    price: "680 DA",
+    image: "https://i.imgur.com/rv9RLuB.jpeg",
     descp:
       "'White Nights' by Fyodor Dostoevsky is a poignant tale of love, loneliness, and fleeting connections. Set against the dreamy backdrop of St. Petersburg, it follows a solitary dreamer who forms a brief yet profound bond with a young woman. This timeless novella captures the beauty and heartbreak of human connection in Dostoevsky’s signature lyrical style.",
     tags: ["Romance", "Fiction", "Russian Literature"],
   },
-
+  {
+    name: "Fabricant de larmes - partie 1",
+    author: "Erin Doom",
+    price: "1100 DA",
+    image: "https://i.imgur.com/ylXhQzO.jpeg",
+    description:
+      "Nica, une jeune orpheline de 17 ans, voit son rêve se réaliser lorsqu’elle est adoptée par une famille. Elle quitte l’orphelinat et ses légendes inquiétantes, notamment celle du « Fabricant de larmes ». Cependant, sa nouvelle vie est perturbée par Rigel, un autre orphelin adopté par la même famille, qui semble déterminé à rendre son quotidien infernal.",
+    tags: ["Fiction", "Romance", "Fantasy"],
+  },
+  {
+    name: "Fabricant de larmes - partie 2",
+    author: "Erin Doom",
+    price: "1100 DA",
+    image: "https://i.imgur.com/frN9bU7.jpeg",
+    description:
+      "La relation conflictuelle entre Nica et Rigel s’intensifie, oscillant entre méfiance et sentiments inavoués. Tandis que des secrets de leur passé resurgissent, les deux jeunes découvrent que leur histoire est plus liée à la sombre légende qu’ils ne l’auraient imaginé.",
+    tags: ["Fiction", "Romance", "Fantasy"],
+  },
 ];
