2587bc29b2550e113ecf019f4b50a66649dc28fb
Solaire <136970695+solxire88@users.noreply.github.com>
2024-11-30 15:16:20 +0100

    Update data2.js

--- src/data2.js
+++ src/data2.js
@@ -2,7 +2,7 @@ export const data2 = [
   {
     Book: "Grokking Algorithms",
     author: "Aditya Y. Bhargava",
-    price: "1150 DA",
+    price: "1250 DA", // updated price
     image: "https://i.imgur.com/CjdhieD.jpeg",
     descp:
       "A friendly, fully-illustrated introduction to the most important computer programming algorithms.",
@@ -11,25 +11,25 @@ export const data2 = [
   {
     Book: "Clean Code",
     author: "Robert C. Martin",
-    price: "1250 DA",
+    price: "1550 DA", // updated price
     image: "https://i.imgur.com/7ex7fXP.jpeg",
     descp:
-      "Even bad code can function. But if code isn’t clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesn’t have to be that way. ",
+      "Even bad code can function. But if code isn’t clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesn’t have to be that way.",
     tags: ["Software Engineering", "Computer Science"],
   },
   {
     Book: "Structure And Interpretation Of C. Programs",
     author: "Harold Abelson (MIT)",
-    price: "1250 DA",
+    price: "1550 DA", // updated price
     image: "https://i.imgur.com/2fflwra.jpeg",
     descp:
       "Structure and Interpretation of Computer Programs has had a dramatic impact on computer science curricula over the past decade. This long-awaited revision contains changes throughout the text.",
-    tags: ["Software Engirneering", "Computer Science"],
+    tags: ["Software Engineering", "Computer Science"],
   },
   {
     Book: "OpenIntro Statistics",
     author: "David M. Diez",
-    price: "1150 DA",
+    price: "1250 DA", // updated price
     image: "https://i.imgur.com/NkwmWRi.jpg",
     descp:
       "OpenIntro Statistics is a beginner-friendly textbook that introduces key statistical concepts like probability, data analysis, and hypothesis testing. It focuses on real-world applications and uses practical examples to enhance understanding. The book also emphasizes the use of statistical software for data analysis.",
@@ -45,12 +45,12 @@ export const data2 = [
     tags: ["Artificial Intelligence", "Data Science", "Computer Science"],
   },
   {
-    "Book": "Aide-Mémoire Mathématiques",
-    "author": "Dunod",
-    "price": "1150 DA",
-    "image": "https://i.imgur.com/qDpZoez.png",
-    "descp":
+    Book: "Aide-Mémoire Mathématiques",
+    author: "Dunod",
+    price: "1250 DA", // updated price
+    image: "https://i.imgur.com/qDpZoez.png",
+    descp:
       "Un guide pratique qui résume les bases des mathématiques, couvrant l’algèbre, l’analyse, la géométrie, les probabilités et les statistiques. Clair et compact, il est parfait pour réviser ou comprendre rapidement les notions essentielles.",
-    "tags": ["Mathématique", "Prépa", "Licence"]
-},
+    tags: ["Mathématique", "Prépa", "Licence"],
+  },
 ];
