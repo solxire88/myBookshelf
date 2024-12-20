d56af8494f3b762a9c2d455ca56e1a5be215eac8
Solaire <136970695+solxire88@users.noreply.github.com>
2024-12-20 14:11:04 +0100

    Update newData.js

--- src/newData.js
+++ src/newData.js
@@ -2,7 +2,7 @@ export const data = [
   {
     Book: "On The Shortness Of Life",
     author: "Seneca",
-    price: "680 DA",
+    price: "780 DA",
     image: "https://i.imgur.com/9PVxlHh.jpeg",
     descp:
       "The Stoic writings of the philosopher Seneca, who lived from c. 5 BC to AD 65, offer powerful insights into the art of living, the importance of reason and morality, and continue to provide.",
@@ -11,7 +11,7 @@ export const data = [
   {
     Book: "Discourses and Selected Writings",
     author: "Epictetus",
-    price: "950 DA",
+    price: "1050 DA",
     image: "https://i.imgur.com/htuilJO.jpeg",
     descp:
       "A new translation of the influential teachings of the great Stoic philosopher.",
@@ -20,7 +20,7 @@ export const data = [
   {
     Book: "Meditations",
     author: "Marcus Aurelius",
-    price: "950 DA",
+    price: "1050 DA",
     image: "https://i.imgur.com/z0OZuGM.jpeg",
     descp:
       "A clear and eloquent leading translation of the Roman emperor’s reflections on human life and death.",
@@ -29,7 +29,7 @@ export const data = [
   {
     Book: "Letters From A Stoic",
     author: "Seneca",
-    price: "950 DA",
+    price: "1050 DA",
     image: "https://i.imgur.com/lcGoYoy.jpeg",
     descp:
       '"It is philosophy that has the duty of protecting us...without it no one can lead a life free of fear or worry."',
@@ -38,7 +38,7 @@ export const data = [
   {
     Book: "Pensées Pour Moi Même",
     author: "Marc Aurèle",
-    price: "950 DA",
+    price: "1050 DA",
     image: "https://i.imgur.com/lVEnUVd.png",
     descp:
       "Pensées pour moi-même est un recueil de réflexions intimes de Marc Aurèle, où il explore des idées stoïciennes sur la maîtrise de soi, l'acceptation du destin et la quête de la vertu. Ce texte offre des conseils pratiques pour vivre avec sagesse, sérénité et justice face aux défis de la vie.",
@@ -47,7 +47,7 @@ export const data = [
   {
     Book: "Meditations On First Philosophy",
     author: "Rene Descartes",
-    price: "680 DA",
+    price: "780 DA",
     image: "https://i.imgur.com/Fkpjf7f.jpg",
     descp:
       "Descartes's Meditations on First Philosophy, the fundamental and originating work of the modern era in Western philosophy, is presented here in Donald Cress's completely revised edition of his well-established translation, bringing this version even closer to Descartes's original, while maintaining its clear and accessible style.",
@@ -56,7 +56,7 @@ export const data = [
   {
     Book: "El Escape Cubano",
     author: "Mira Canion",
-    price: "680 DA",
+    price: "780 DA",
     image: "https://i.imgur.com/uNPpT5N.jpg",
     descp:
       "Inspired by a true story, El Escape Cubano is a first-person narrative reader with 140 unique words. In a matter of minutes, Miguel's happiness changes to gripping fear when he overhears his parents fighting about escaping from Cuba on a raft.",
@@ -65,7 +65,7 @@ export const data = [
   {
     Book: "Beyond Good and Evil",
     author: "Friedrich Nietzsche",
-    price: "950 DA",
+    price: "1050 DA",
     image: "https://i.imgur.com/4qT4bOh.jpg",
     descp:
       "Nietzsche's exploration of morality and the concept of 'beyond good and evil', focusing on individual freedom, power, and societal values.",
@@ -74,7 +74,7 @@ export const data = [
   {
     Book: "The Almanack Of Naval Ravikant",
     author: "Eric Jorgenson",
-    price: "950 DA",
+    price: "1050 DA",
     image: "https://i.imgur.com/iVPC41B.jpeg",
     descp:
       "The Almanack of Naval Ravikant offers a collection of insights and principles from the entrepreneur and philosopher on how to build wealth and cultivate long-term happiness, emphasizing that these are learnable skills rather than mere luck or innate traits.",
@@ -83,7 +83,7 @@ export const data = [
   {
     Book: "The Seven Year Slip",
     author: "Eric Jorgenson",
-    price: "950 DA",
+    price: "1050 DA",
     image: "https://i.imgur.com/lIgqsHm.jpeg",
     descp:
       "An overworked book publicist with a perfectly planned future hits a snag when she falls in love with her temporary roommate…only to discover he lives seven years in the past, in this witty and wise new novel from the New York Times bestselling author of The Dead Romantics.",
@@ -92,7 +92,7 @@ export const data = [
   {
     Book: "Le Manuel",
     author: "Épictète",
-    price: "680 DA",
+    price: "780 DA",
     image: "https://i.imgur.com/sccEggv.png",
     descp:
       "'Le Manuel d'Épictète' est un guide stoïcien qui enseigne à vivre en paix avec soi-même et le monde, en se concentrant sur ce qui dépend de nous (pensées, actions) et en acceptant ce qui échappe à notre contrôle. Un classique de sagesse et de sérénité.",
@@ -101,7 +101,7 @@ export const data = [
   {
     Book: "This Is Marketing",
     author: "Seth Goden",
-    price: "950 DA",
+    price: "1050 DA",
     image: "https://i.imgur.com/wz2XFLa.png",
     descp:
       "'This Is Marketing' by Seth Godin is a must-read for anyone looking to create impactful, customer-focused marketing. It highlights the importance of building trust, understanding your audience, and offering value that truly resonates. With practical insights, it shifts the focus from mass appeal to meaningful connections that drive lasting change.",
@@ -110,7 +110,7 @@ export const data = [
   {
     Book: "White Nights",
     author: "Fyodor Dostoyevsky",
-    price: "680 DA",
+    price: "780 DA",
     image: "https://i.imgur.com/rv9RLuB.jpeg",
     descp:
       "'White Nights' by Fyodor Dostoevsky is a poignant tale of love, loneliness, and fleeting connections. Set against the dreamy backdrop of St. Petersburg, it follows a solitary dreamer who forms a brief yet profound bond with a young woman. This timeless novella captures the beauty and heartbreak of human connection in Dostoevsky’s signature lyrical style.",
@@ -119,7 +119,7 @@ export const data = [
   {
     Book: "Fabricant de larmes - partie 1",
     author: "Erin Doom",
-    price: "1100 DA",
+    price: "1200 DA",
     image: "https://i.imgur.com/ylXhQzO.jpeg",
     descp:
       "Nica, une jeune orpheline de 17 ans, voit son rêve se réaliser lorsqu’elle est adoptée par une famille. Elle quitte l’orphelinat et ses légendes inquiétantes, notamment celle du « Fabricant de larmes ». Cependant, sa nouvelle vie est perturbée par Rigel, un autre orphelin adopté par la même famille, qui semble déterminé à rendre son quotidien infernal.",
@@ -128,7 +128,7 @@ export const data = [
   {
     Book: "Fabricant de larmes - partie 2",
     author: "Erin Doom",
-    price: "1100 DA",
+    price: "1200 DA",
     image: "https://i.imgur.com/frN9bU7.jpeg",
     descp:
       "La relation conflictuelle entre Nica et Rigel s’intensifie, oscillant entre méfiance et sentiments inavoués. Tandis que des secrets de leur passé resurgissent, les deux jeunes découvrent que leur histoire est plus liée à la sombre légende qu’ils ne l’auraient imaginé.",
@@ -137,7 +137,7 @@ export const data = [
   {
   Book: "The Tearsmith",
   author: "Erin Doom",
-  price: "1350 DA",
+  price: "1450 DA",
   image: "https://i.imgur.com/eJoO48v.jpeg",
   descp: "The Tearsmith by Erin Doom follows Nica, a 17-year-old orphan, whose life changes when she's adopted into a new family. However, her peace is disrupted by Rigel, another adopted orphan, as they unravel secrets tied to a haunting legend.",
   tags: ["Fiction", "Romance", "Netflix"]
@@ -145,7 +145,7 @@ export const data = [
 {
     Book: "Kiara Tome I",
     author: "Hazel Diaz",
-    price: "1350 DA",
+    price: "1450 DA",
     image: "https://i.imgur.com/5hsWxaP.jpeg",
     descp: "Kiara par Hazel Diaz, Tome 1, suit Kiara, une jeune femme au passé mystérieux, qui se lance dans un voyage de découverte de soi et d’aventure. Alors qu'elle navigue dans un monde rempli de dangers et d’intrigues, Kiara découvre des pouvoirs cachés en elle et des secrets sombres qui pourraient bouleverser tout ce qu'elle connaît.",
     tags: ["Fiction", "Adventure"]
@@ -153,7 +153,7 @@ export const data = [
 {
     Book: "Kiara Tome II",
     author: "Hazel Diaz",
-    price: "1350 DA",
+    price: "1450 DA",
     image: "https://i.imgur.com/DaK6LOz.jpeg",
     descp: "Kiara par Hazel Diaz, Tome 2, poursuit l’aventure de Kiara, qui, après avoir découvert ses pouvoirs, se retrouve confrontée à de nouvelles menaces et à des alliances dangereuses. Alors que les mystères autour de son passé s’éclaircissent, elle doit faire face à des choix difficiles qui pourraient changer le cours de son destin.",
     tags: ["Fiction", "Adventure"]
