09e9f999e74167069377f61ac6e2a6680f31c290
Solaire <136970695+solxire88@users.noreply.github.com>
2024-12-20 14:09:54 +0100

    Update data2.js

--- src/data2.js
+++ src/data2.js
@@ -2,7 +2,7 @@ export const data2 = [
   {
     Book: "Grokking Algorithms",
     author: "Aditya Y. Bhargava",
-    price: "1250 DA", // updated price
+    price: "1350 DA", // updated price
     image: "https://i.imgur.com/CjdhieD.jpeg",
     descp: "A friendly, fully-illustrated introduction to the most important computer programming algorithms.",
     tags: ["Computer Science", "DSA"],
@@ -10,7 +10,7 @@ export const data2 = [
   {
     Book: "Clean Code",
     author: "Robert C. Martin",
-    price: "1550 DA", // updated price
+    price: "1650 DA", // updated price
     image: "https://i.imgur.com/7ex7fXP.jpeg",
     descp: "Even bad code can function. But if code isn’t clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesn’t have to be that way.",
     tags: ["Software Engineering", "Computer Science"],
@@ -18,7 +18,7 @@ export const data2 = [
   {
     Book: "Structure And Interpretation Of C. Programs",
     author: "Harold Abelson (MIT)",
-    price: "1550 DA", // updated price
+    price: "1650 DA", // updated price
     image: "https://i.imgur.com/2fflwra.jpeg",
     descp: "Structure and Interpretation of Computer Programs has had a dramatic impact on computer science curricula over the past decade. This long-awaited revision contains changes throughout the text.",
     tags: ["Software Engineering", "Computer Science"],
@@ -26,7 +26,7 @@ export const data2 = [
   {
     Book: "OpenIntro Statistics",
     author: "David M. Diez",
-    price: "1250 DA", // updated price
+    price: "1350 DA", // updated price
     image: "https://i.imgur.com/NkwmWRi.jpg",
     descp: "OpenIntro Statistics is a beginner-friendly textbook that introduces key statistical concepts like probability, data analysis, and hypothesis testing. It focuses on real-world applications and uses practical examples to enhance understanding. The book also emphasizes the use of statistical software for data analysis.",
     tags: ["Statistics", "Data Science"],
@@ -34,7 +34,7 @@ export const data2 = [
   {
     Book: "The Hundred-Page Machine Learning Book",
     author: "Andriy Burkov",
-    price: "1050 DA",
+    price: "1150 DA",
     image: "https://i.imgur.com/nQCNCWF.jpg",
     descp: "Concise and to the point — the book can be read during a week. During that week, you will learn almost everything modern machine learning has to offer. The author and other practitioners have spent years learning these concepts.",
     tags: ["Artificial Intelligence", "Data Science", "Computer Science"],
@@ -42,7 +42,7 @@ export const data2 = [
   {
     Book: "Aide-Mémoire Mathématiques",
     author: "Dunod",
-    price: "1250 DA", // updated price
+    price: "1350 DA", // updated price
     image: "https://i.imgur.com/qDpZoez.png",
     descp: "Un guide pratique qui résume les bases des mathématiques, couvrant l’algèbre, l’analyse, la géométrie, les probabilités et les statistiques. Clair et compact, il est parfait pour réviser ou comprendre rapidement les notions essentielles.",
     tags: ["Mathématique", "Prépa", "Licence"],
@@ -50,7 +50,7 @@ export const data2 = [
   {
     Book: "Aide-Mémoire Composants Électroniques",
     author: "Dunod",
-    price: "1250 DA",
+    price: "1350 DA",
     image: "https://i.imgur.com/lOuTy7O.jpeg",
     descp: "Aide-mémoire Composants électroniques 5e édition est un guide pratique et concis sur les composants électroniques, leurs caractéristiques, et applications, idéal pour les étudiants et professionnels en électronique.",
     tags: ["Licence", "Electronique", "Prépa"]
@@ -58,7 +58,7 @@ export const data2 = [
   {
     Book: "Mini Manuel Mecanique Du Point",
     author: "Dunod",
-    price: "1050 DA",
+    price: "1150 DA",
     image: "https://i.imgur.com/Yx3KBYJ.jpeg",
     descp: "Mini Manuel Mécanique du Point est un guide compact qui explique de manière claire les bases de la mécanique du point, incluant principes, théorèmes et applications. Parfait pour les étudiants en sciences et ingénierie.",
     tags: ["Licence", "Physique", "Prépa"]
@@ -66,7 +66,7 @@ export const data2 = [
   {
     Book: "Cours de Physique : Mécanique du Point",
     author: "Dunod",
-    price: "1250 DA",
+    price: "1350 DA",
     image: "https://i.imgur.com/k6DjA9f.jpeg",
     descp: "Cours de Physique : Mécanique du Point par Dunod est un ouvrage pédagogique qui couvre les principes fondamentaux de la mécanique du point. Il offre des explications détaillées, des exemples concrets et des exercices corrigés, idéal pour les étudiants en physique et ingénierie.",
     tags: ["Licence", "Prépa", "Physique"]
@@ -74,7 +74,7 @@ export const data2 = [
   {
     Book: "Mini Manuel De Chimie Organique",
     author: "Dunod",
-    price: "1250 DA",
+    price: "1350 DA",
     image: "https://i.imgur.com/2b3u7Qe.jpeg",
     descp: "Mini Manuel de Chimie Organique est un ouvrage concis et pédagogique qui couvre les notions clés de la chimie organique, avec des exemples et exercices pour faciliter l’apprentissage. Parfait pour les étudiants en sciences.",
     tags: ["Licence", "Prépa", "Chimie"]
@@ -82,7 +82,7 @@ export const data2 = [
   {
     Book: "Mini Manuel d'Architecture de l'Ordinateur",
     author: "Dunod",
-    price: "1050 DA",
+    price: "1150 DA",
     image: "https://i.imgur.com/Ip5xmZq.jpeg",
     descp: "Mini Manuel d'Architecture de l'Ordinateur est un guide synthétique qui explique les bases de l'architecture des ordinateurs, des composants matériels aux principes de fonctionnement. Idéal pour les étudiants en informatique et électronique.",
     tags: ["Licence", "Prépa", "Informatique"]
@@ -90,7 +90,7 @@ export const data2 = [
   {
     Book: "Mini Manuel de C++",
     author: "Dunod",
-    price: "1050 DA",
+    price: "1150 DA",
     image: "https://i.imgur.com/GvROGTu.jpeg",
     descp: "Mini Manuel de C++ est un guide clair et concis qui couvre les bases de la programmation en C++, avec des exemples pratiques et des exercices. Idéal pour les débutants et étudiants en informatique.",
     tags: ["Licence", "Prépa", "Informatique"]
@@ -98,7 +98,7 @@ export const data2 = [
   {
     Book: "Introduction To Statistical Learning",
     author: "Trevor Hastie & Rob Tibshirani",
-    price: "1550 DA",
+    price: "1650 DA",
     image: "https://i.imgur.com/nRtn8R8.jpeg",
     descp: "Introduction to Statistical Learning is a comprehensive textbook that provides an accessible overview of statistical and machine learning methods. It covers fundamental topics such as regression, classification, resampling, and tree-based methods, with practical examples using R. Ideal for beginners and professionals in data science and analytics.",
     tags: ["Data Science", "Machine Learning", "Statistics"]
@@ -106,7 +106,7 @@ export const data2 = [
   {
     Book: "Mini Manuel d'Électromagnétisme",
     author: "Dunod",
-    price: "1050 DA",
+    price: "1150 DA",
     image: "https://i.imgur.com/YSc5TgL.jpeg",
     descp: "Mini Manuel d'Électromagnétisme est un guide synthétique qui explique les principes fondamentaux de l’électromagnétisme, avec des exemples pratiques et des exercices corrigés. Idéal pour les étudiants en physique et ingénierie.",
     tags: ["Licence", "Prépa", "Électricité"]
@@ -114,7 +114,7 @@ export const data2 = [
   {
     Book: "Mini Manuel de Finance d'Entreprise",
     author: "Dunod",
-    price: "1050 DA",
+    price: "1150 DA",
     image: "https://i.imgur.com/QiBGL0X.jpeg",
     descp: "Mini Manuel de Finance d'Entreprise est un guide concis qui présente les bases de la gestion financière, de l'analyse des investissements à la gestion des ressources. Idéal pour les étudiants et professionnels en finance.",
     tags: ["Licence", "Prépa", "Finance"]
@@ -122,7 +122,7 @@ export const data2 = [
   {
     Book: "Mini Manuel d'Analyse",
     author: "Dunod",
-    price: "1050 DA",
+    price: "1150 DA",
     image: "https://i.imgur.com/D7Xn5gn.jpeg",
     descp: "Mini Manuel d'Analyse est un guide clair et synthétique couvrant les concepts fondamentaux de l’analyse mathématique, avec des exemples et exercices corrigés. Parfait pour les étudiants en mathématiques et sciences.",
     tags: ["Licence", "Prépa", "Mathématiques"]
@@ -130,7 +130,7 @@ export const data2 = [
   {
     Book: "Mini Manuel de Mathématiques Financières",
     author: "Dunod",
-    price: "1050 DA",
+    price: "1150 DA",
     image: "https://i.imgur.com/x2vtfz0.jpeg",
     descp: "Mini Manuel de Mathématiques Financières est un guide pratique et concis qui aborde les concepts clés des mathématiques appliquées à la finance, avec des exemples concrets et exercices. Idéal pour étudiants et professionnels en finance.",
     tags: ["Licence", "Prépa", "Mathématiques"]
@@ -138,7 +138,7 @@ export const data2 = [
   {
     Book: "Mini Manuel de Comptabilité de Gestion",
     author: "Dunod",
-    price: "1250 DA",
+    price: "1350 DA",
     image: "https://i.imgur.com/e7ayLtH.jpeg",
     descp: "Mini Manuel de Comptabilité de Gestion est un guide pratique qui présente les principes essentiels de la comptabilité de gestion, de l'analyse des coûts à la prise de décision financière. Idéal pour les étudiants et professionnels en gestion.",
     tags: ["Licence", "Prépa", "Comptabilité"]
@@ -146,7 +146,7 @@ export const data2 = [
   {
   Book: "Bactériologie Médicale",
   author: "Elsevier",
-  price: "1550 DA",
+  price: "1650 DA",
   image: "https://i.imgur.com/mAKhflo.jpeg",
   descp: "Ce livre est divisé en deux parties - \"Bactériologie Médicale\" d'Elsevier est un ouvrage de référence qui couvre les bases de la bactériologie appliquée à la médecine. Il explore les caractéristiques des bactéries, les infections qu'elles causent et les méthodes de diagnostic et de traitement. Essentiel pour les étudiants et professionnels de la santé.",
   tags: ["Médecine", "Pharmacie", "Résidanat"]
